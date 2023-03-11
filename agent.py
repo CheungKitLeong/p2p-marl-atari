cf92bc85761ef7df5e864cdf27cb73b737daa0ba
cheung <cheungkitleong@gmail.com>
2023-03-11 19:11:10 +0800

    minor

--- agent.py
+++ agent.py
@@ -107,9 +107,9 @@ class Agent:
 
         # Save the model dict
         # Create folder to save models
-        if self.num_games % 10 == 0:
+        if self.num_games % 20 == 0:
 
-            path = 'models/' + self.path + '/epoch_' + str(self.num_games)
+            path = 'models/' + self.path + '/epoch_' + str(self.num_games) + '.pt'
             save(self.agent_control.moving_nn.state_dict(), path)
 
 
