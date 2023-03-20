cdd2cfbc611c709b828b40dcad5783950bd6fa03
cyrus <cyruswgad@gmail.com>
2023-03-20 14:57:45 +0800

    init

--- evaluation.py
+++ evaluation.py
@@ -18,25 +18,25 @@ DQN_HYPERPARAMS = {
     'n_iter_update_nn': 1000,
 }
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# device = torch.device("mps")
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("mps")
 
-def evaluation(dir1, dir2, interval=20, max_epoch=600, eval_num=100, MAX_STEP=10000):
-    env = make_pong()
-    def fixed_opponent(fixed_player_epoch=200):
+def evaluation(dir1, dir2, interval=20, max_epoch=20, eval_num=1, MAX_STEP=10000):
+    env = make_pong(True)
+    def fixed_opponent(fixed_player_epoch=300):
         fixed_r = []
         current_player = 0
         fixed_agent_path = agent1_dir + 'epoch_{}.pt'.format(fixed_player_epoch)
         fixed_agent = Agent(env, DQN_HYPERPARAMS, device, 'first_0', fixed_agent_path)
 
         while current_player <= max_epoch:
-            agents = [fixed_agent, Agent(env, DQN_HYPERPARAMS, device, 'second_0', agent2_dir + 'epoch_{}.pt'.format(current_player))]
+            agents = [fixed_agent, Agent(env, DQN_HYPERPARAMS, device, 'second_0', agent2_dir + 'epoch_{}.pt'.format(600))]
             r = []
             for num in range(eval_num):
-                print('fixed ,current_player:', current_player, 'eval_num:', eval_num)
+                print('fixed ,current_player:', current_player, 'eval_num:', num)
                 env.reset()
-                env.step(1)
-                env.step(1)
+                # env.step(4)
+                # env.step(4)
 
                 for i in range(MAX_STEP):
                     for n in range(2):
@@ -46,9 +46,10 @@ def evaluation(dir1, dir2, interval=20, max_epoch=600, eval_num=100, MAX_STEP=10
                         if(env.last()[1] > 0):
                             r.append(n)
 
-                    if reward > 0 or reward < 0 and not done and not trunc:
-                        env.step(1)
-                        env.step(1)
+                        # if reward > 0 or reward < 0 and not done and not trunc:
+                        #     print(reward)
+                        #     env.step(1)
+                        #     env.step(1)
 
                     if done or trunc:
                         if trunc:
@@ -57,7 +58,6 @@ def evaluation(dir1, dir2, interval=20, max_epoch=600, eval_num=100, MAX_STEP=10
             print('fixed ,current_player:', current_player, 'mean:', np.mean(r))
             fixed_r.append(r)
             current_player += interval
-
         return fixed_r
 
     def adjacent_opponent():
@@ -71,24 +71,22 @@ def evaluation(dir1, dir2, interval=20, max_epoch=600, eval_num=100, MAX_STEP=10
                 ]
             r = []
             for num in range(eval_num):
-                print('adjacent ,current_player:', current_player, 'eval_num:', eval_num)
+                print('adjacent ,current_player:', current_player, 'eval_num:', num)
                 env.reset()
-                env.step(1)
-                env.step(1)
+                env.step(4)
+                env.step(4)
 
                 for i in range(MAX_STEP):
                     for n in range(2):
                         obs, reward, done, trunc, info = env.last()
                         action = agents[n].select_greedy_action(obs)
-                        print(action)
-                        input()
                         env.step(action)
                         if(env.last()[1] > 0):
                             r.append(n)
 
                     if reward > 0 or reward < 0 and not done and not trunc:
-                        env.step(1)
-                        env.step(1)
+                        env.step(4)
+                        env.step(4)
 
                     if done or trunc:
                         if trunc:
@@ -100,9 +98,7 @@ def evaluation(dir1, dir2, interval=20, max_epoch=600, eval_num=100, MAX_STEP=10
 
         return adjacent_r
 
-    print('Started fixed evaluation...')
     fixed = fixed_opponent()
-    print('Started adjacent evaluation...')
     adjacent = adjacent_opponent()
     return fixed, adjacent
 
@@ -110,18 +106,25 @@ def process(agent1_dir, agent2_dir):
     data = evaluation(agent1_dir, agent2_dir)
 
     f1 = open('fixed.csv', 'w')
-    f1.write(str(data[0]).replace('[', '\n').replace(']', ''))
+    f1.write(str(data[0]).replace('[[', '').replace('[', '\n').replace(']', ''))
     f1.close()
 
     f2 = open('adjacent.csv', 'w')
-    f2.write(str(data[1]).replace('[', '\n').replace(']', ''))
+    f2.write(str(data[1]).replace('[[', '').replace('[', '\n').replace(']', ''))
     f2.close()
 
-    fixed = np.array(data[0])
-    adjacent = np.array(data[1])
+    fixed_mean = []
+    for n in data[0]:
+        fixed_mean.append(np.mean(n))
 
-    fixed_mean = np.mean(fixed, axis=1)
-    adjacent_mean = np.mean(adjacent, axis=1)
+    adjacent_mean = []
+    for n in data[1]:
+        adjacent_mean.append(np.mean(n))
+
+    fixed_mean = np.array(fixed_mean)
+    adjacent_mean = np.array(adjacent_mean)
+
+
 
     plt.plot([i*20 for i in range(len(fixed_mean))], fixed_mean)
     plt.xlabel('model')
